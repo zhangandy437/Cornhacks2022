a8c38ec60f713993c1eedfa3d9d2851e2e84537c
Andy Zhang <zhangandy0001@gmail.com>
2022-01-22 15:18:33 -0600

    hello

--- mysite/youtube/quickstart.py
+++ mysite/youtube/quickstart.py
@@ -2,10 +2,13 @@ from __future__ import unicode_literals
 from pyyoutube import Api
 from random import random
 import youtube_dl
+from os import listdir
+from os.path import isfile, join
+from youtube_dl.utils import DownloadError
 
 api = Api(api_key="AIzaSyDQgrNtKZlwTQbXVy_JbspnqHz-XWyFzX4")
 
-
+# def downloadmp3():
 playlists = api.get_playlist_items(playlist_id="PLWOGZHNCc5NnJox5IaqDSMUmMKRCT7Ao9")
 
 # for item in playlists.items:
@@ -31,7 +34,11 @@ ydl_opts = {
         'preferredquality': '192',
     }],
 }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=DvOq4plWHjg&list=PLWOGZHNCc5NnJox5IaqDSMUmMKRCT7Ao9&index=13'])
-    
+try:
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['https://www.youtube.com/watch?v=DvOq4plWHjg&list=PLWOGZHNCc5NnJox5IaqDSMUmMKRCT7Ao9&index=13'])
+except DownloadError:
+    print("welp")
+print('hi')
+files = [f for f in listdir('.') if isfile(join('.', f))]
     
--- mysite/youtube/views.py
+++ mysite/youtube/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import FileResponse
 from random import random
+from . import quickstart
 
 import os
 
@@ -10,55 +11,5 @@ import googleapiclient.errors
 
 # Create your views here.
 def index(request):
-    data = {
-        'url': randomyoutube()
-    }
-    return JsonResponse(data)
-
-
-def randomyoutube() -> str:
-    urls = readTxt()
-    return urls[int(random() * len(urls))]
-
-def readTxt():
-    f = open('youtube/urls.txt', "r")
-    lines = f.read().splitlines()
-    f.close()
-    return [i.split(' ') for i in lines]
-
-
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/guides/code_samples#python
-
-
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    request = youtube.channels().list(
-        
-    )
-    response = request.execute()
-
-    print(response)
-
-if __name__ == "__main__":
-    main()
+    filename = quickstart.downloadrandom()
+    
