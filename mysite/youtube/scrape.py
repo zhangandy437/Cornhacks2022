dc32dec39ad9bc2f1321a5f3a3dee43e47b89f86
Andy Zhang <zhangandy0001@gmail.com>
2022-01-22 20:56:17 -0600

    finished video stitch

--- mysite/youtube/scrape.py
+++ mysite/youtube/scrape.py
@@ -1,14 +1,95 @@
-import requests
+import json
+from urllib.error import HTTPError, URLError
+import requests, re
 from bs4 import BeautifulSoup
+import urllib.request
 
-def scrape(URL = "https://www.reuters.com/world/middle-east/turkish-journalist-arrested-insulting-erdogan-cnn-turk-2022-01-22/"):
-    page = requests.get(URL)
-    print('Scraping for images...')
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="fusion-app")
-    job_elements = results.find_all("img")
-    images = []
-    for element in job_elements:
-        print(element["src"])
-        images.append(element['src'])
-    return images
+headers = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+}
+
+params = {
+    "q": "vaporwave",
+    "tbm": "isch",
+    "ijn": "0",
+}
+
+html = requests.get("https://www.google.com/search", params=params, headers=headers)
+soup = BeautifulSoup(html.text, 'lxml')
+
+headers = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+}
+
+params = {
+    "q": "minecraft shaders 8k photo",
+    "tbm": "isch",
+    "ijn": "0",
+}
+
+html = requests.get("https://www.google.com/search", params=params, headers=headers)
+
+def get_images_data():
+
+    print('\nGoogle Images Metadata:')
+    for google_image in soup.select('.isv-r.PNCib.MSM1fd.BUooTd'):
+        title = google_image.select_one('.VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb')['title']
+        source = google_image.select_one('.fxgdke').text
+        link = google_image.select_one('.VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb')['href']
+        print(f'{title}\n{source}\n{link}\n')
+
+    # this steps could be refactored to a more compact
+    all_script_tags = soup.select('script')
+
+    # # https://regex101.com/r/48UZhY/4
+    matched_images_data = ''.join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
+
+    # https://kodlogs.com/34776/json-decoder-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
+    # if you try to json.loads() without json.dumps it will throw an error:
+    # "Expecting property name enclosed in double quotes"
+    matched_images_data_fix = json.dumps(matched_images_data)
+    matched_images_data_json = json.loads(matched_images_data_fix)
+
+    # https://regex101.com/r/pdZOnW/3
+    matched_google_image_data = re.findall(r'\[\"GRID_STATE0\",null,\[\[1,\[0,\".*?\",(.*),\"All\",', matched_images_data_json)
+
+    # https://regex101.com/r/NnRg27/1
+    matched_google_images_thumbnails = ', '.join(
+        re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
+                   str(matched_google_image_data))).split(', ')
+
+    print('Google Image Thumbnails:')  # in order
+    for fixed_google_image_thumbnail in matched_google_images_thumbnails:
+        # https://stackoverflow.com/a/4004439/15164646 comment by Frédéric Hamidi
+        google_image_thumbnail_not_fixed = bytes(fixed_google_image_thumbnail, 'ascii').decode('unicode-escape')
+
+        # after first decoding, Unicode characters are still present. After the second iteration, they were decoded.
+        google_image_thumbnail = bytes(google_image_thumbnail_not_fixed, 'ascii').decode('unicode-escape')
+        print(google_image_thumbnail)
+
+    # removing previously matched thumbnails for easier full resolution image matches.
+    removed_matched_google_images_thumbnails = re.sub(
+        r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]', '', str(matched_google_image_data))
+
+    # https://regex101.com/r/fXjfb1/4
+    # https://stackoverflow.com/a/19821774/15164646
+    matched_google_full_resolution_images = re.findall(r"(?:'|,),\[\"(https:|http.*?)\",\d+,\d+\]",
+                                                       removed_matched_google_images_thumbnails)
+
+    out = []
+    print('\nFull Resolution Images:')  # in order
+    for index, fixed_full_res_image in enumerate(matched_google_full_resolution_images):
+        # https://stackoverflow.com/a/4004439/15164646 comment by Frédéric Hamidi
+        original_size_img_not_fixed = bytes(fixed_full_res_image, 'ascii').decode('unicode-escape')
+        original_size_img = bytes(original_size_img_not_fixed, 'ascii').decode('unicode-escape')
+        print(original_size_img)
+        try:
+            urllib.request.urlretrieve(original_size_img, f"imgs/{index}.jpg")
+            out.append(f'imgs/{index}.jpg')
+        except (HTTPError, URLError) as e:
+            print("o well")
+    
+    return out
+# get_images_data()
--- mysite/youtube/views.py
+++ mysite/youtube/views.py
@@ -1,19 +1,60 @@
 from django.http import HttpResponse, response
-
-from . import quickstart
-
+from . import quickstart, scrape
 import os
+from moviepy.editor import AudioFileClip, ImageClip, VideoClip, VideoFileClip
+from PIL import Image, UnidentifiedImageError
 
+IMG_PATH = 'imgs'
 
 # Create your views here.
 def index(request):
     filename = quickstart.downloadrandom()
     file = open(filename, "rb").read()
-    # return FileResponse(file)
-    # file = open("/path/to/your/song.mp3", "rb").read() 
+
     response = HttpResponse()
     response.write(file)
     # response['Content-Type'] = 'audio/mp3'
     response['Content-Disposition'] = f'attachment; filename={filename}' 
     quickstart.cleanup(filename)
     return response
+
+def stitch(request):
+    # scrap images
+    print("--- Getting audio...")
+    audio_path = quickstart.downloadrandom()
+    audio = AudioFileClip(audio_path)
+    
+    print('--- Getting images...')
+    photos = scrape.get_images_data()
+    fphotos = []
+    for photo in photos:
+        try:
+            fphotos.append(Image.open(photo).resize((800,800), Image.ANTIALIAS))
+        except UnidentifiedImageError:
+            print(end='')
+    
+    print('--- Making gif...')
+    duration = audio.duration // len(fphotos) * 1000
+    fphotos[0].save(IMG_PATH + '/temp.gif', save_all=True, append_images=fphotos[1:], duration=duration)
+    img = VideoFileClip(IMG_PATH + '/temp.gif')
+    
+    print('--- Creating vid...')
+    vid = img.set_audio(audio)
+    vid.duration = audio.duration
+    vid.write_videofile("test.mp4", fps=60)
+    
+    file = open('test.mp4', "rb").read()
+    
+    response = HttpResponse()
+    response.write(file)
+    # response['Content-Type'] = 'audio/mp3'
+    response['Content-Disposition'] = f'attachment; filename=vid.mp4' 
+    # cleanup
+    cleanup()
+    quickstart.cleanup(audio_path)
+    return response
+
+def cleanup(imgs):
+    for img in imgs:
+        os.remove(img)
+    os.remove('test.mp4')
